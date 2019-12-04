ba0f8ebe75fc5446f0e2ca4a1a721311affa28ff
Oleksii Korniienko <kornienko1296@gmail.com>
2019-12-04 19:18:45 +0200

    add material

--- Library.WebApi/ClientApp/src/app/app.module.ts
+++ Library.WebApi/ClientApp/src/app/app.module.ts
@@ -9,9 +9,12 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModulesModule } from './material-modules/material-modules.module';
 
 @NgModule({
   declarations: [
+    MaterialModulesModule,
     AppComponent,
     NavMenuComponent,
     HomeComponent,
@@ -26,7 +29,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
