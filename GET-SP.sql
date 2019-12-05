a2b0319e7dff31ad7c568d1e80b30eaa3beb1a8f
Maryna Kudriavtseva <art3mida12345@gmail.com>
2019-12-05 22:32:35 +0200

    Books repository works

--- GET-SP.sql
+++ GET-SP.sql
@@ -4,18 +4,19 @@ GO
 CREATE PROCEDURE spGetAllBooks
 AS BEGIN
    SELECT * FROM Books;
-END;
-GO;
+END
+GO
 
 CREATE PROCEDURE spGetAllReaders
 AS BEGIN
    SELECT * FROM Readers;
-END;
-GO;
+END
+GO
 
 CREATE PROCEDURE spGetLibraryCard
 AS BEGIN
    SELECT lc.*, r.FirstName, r.LastName, r.Phone, b.Author, b.Quantity as BookQuantity, b.Title FROM LibraryCards lc
    INNER JOIN Readers r ON r.Id = lc.ReaderId
    INNER JOIN Books b ON b.Id = lc.BookId
-END;
+END
+GO
