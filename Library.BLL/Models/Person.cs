47329d1bfa7de508b054bafa7d762905ed229665
Oleksii Korniienko <kornienko1296@gmail.com>
2019-12-05 18:22:08 +0200

    add initial models

---
+++ Library.BLL/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.BLL.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
