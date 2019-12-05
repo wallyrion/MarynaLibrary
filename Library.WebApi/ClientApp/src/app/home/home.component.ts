2317550f6c5e49bcdf729e776e5b5247eba31418
Oleksii Korniienko <kornienko1296@gmail.com>
2019-12-05 17:44:44 +0200

    fix problem with pagination and sorting

--- Library.WebApi/ClientApp/src/app/home/home.component.ts
+++ Library.WebApi/ClientApp/src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Journal } from '../models/jornal';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+
 })
 export class HomeComponent implements OnInit {
 
